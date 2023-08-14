567e8d9fa8c6710da0015725b1a3656ee0881f57
William Roger <williamroger.frontend@gmail.com>
2023-08-14 14:47:17 -0300

    validando a existencia de cookies

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -2,33 +2,54 @@ import { FastifyInstance } from "fastify"
 import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
 import { knex } from "../database"
+import { checkSessionIdExists } from "../middlewares/check-session-id-exists"
 
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select()
+  app.get('/', {
+    preHandler: [checkSessionIdExists]
+  }, async (request, reply) => {
+    const { sessionId } = request.cookies
+
+    const transactions = await knex('transactions')
+      .where('session_id', sessionId)
+      .select()
 
     return {
       transactions
     }
   })
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', {
+    preHandler: [checkSessionIdExists]
+  }, async (request) => {
+    const { sessionId } = request.cookies
     const getTransactionParamsSchema = z.object({
       id: z.string().uuid(),
     })
 
     const { id } = getTransactionParamsSchema.parse(request.params)
 
-    const transaction = await knex('transactions').where('id', id).first()
+    const transaction = await knex('transactions')
+      .where({
+        session_id: sessionId,
+        id,
+      })
+      .first()
 
     return {
       transaction,
     }
   })
 
-  app.get('/summary', async () => {
-    const summary = await knex('transactions').sum('amount', { as: 'amount' }).first()
+  app.get('/summary', {
+    preHandler: [checkSessionIdExists]
+  }, async (request) => {
+    const { sessionId } = request.cookies
+    const summary = await knex('transactions')
+      .where('session_id', sessionId)
+      .sum('amount', { as: 'amount' })
+      .first()
 
     return {
       summary,
@@ -54,7 +75,7 @@ export async function transactionsRoutes(app: FastifyInstance) {
         maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
       }) 
     }
-    
+
     await knex('transactions').insert({
       id: randomUUID(),
       title,
