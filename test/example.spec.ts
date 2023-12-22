87bdb7c0bb998b0ec64e925aefaf8a0a88d285e0
William Roger <williamroger.frontend@gmail.com>
2023-12-22 00:55:49 -0300

    testando criação de transação

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,10 +1,27 @@
-import { expect, test } from 'vitest';
+import { afterAll, beforeAll, expect, test } from 'vitest';
+import request from 'supertest';
+
+import { app } from '../src/app';
+
+beforeAll(async () => {
+  app.ready()
+})
+
+afterAll(async () => {
+  await app.close()
+})
 
 // -> Descrição
-test('o usuário consegue criar uma nova transação', () => {
+test('user can create a new transaction', async () => {
   // -> Simulação de cenário e Execução
-  const responseStatusCode = 201;
+  const response = await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New Transaction',
+      amount: 5000,
+      type: 'credit',
+    })
 
   // -> Validação
-  expect(responseStatusCode).toEqual(201)
+  expect(response.statusCode).toEqual(201)
 });
